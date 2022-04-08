bbfc99adf46c9f2ef3168872ad4ba2b96b8301fb
eche-unicorn <d.anasteysha@gmail.com>
2022-04-08 17:08:30 +0300

    corr

--- controllers/transactions/addNewTransaction.js
+++ controllers/transactions/addNewTransaction.js
@@ -1,10 +1,22 @@
 const { Transaction, schemas } = require('../../models/transaction');
-const CreateError = require('http-errors')
+
+const CreateError = require('http-errors');
 
 const addNewTransaction = async (req, res, next) => {
   try {
-    // const;
+    const { error } = schemas.add.validate(req.body);
+    if (error) {
+      console.log(error.message);
+      throw new CreateError(400, error.message);
+    }
+
+    const data = { ...req.body, owner: req.user.id };
+    const result = await Transaction.create(data);
+    res.status(201).json(result);
   } catch (error) {
+    if (error.message.toLowerCase().includes('validation failed')) {
+      error.status = 400;
+    }
     next(error);
   }
 };
