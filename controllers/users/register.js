20815e11539a5a032955f338c7cef2fdb42fa516
OlhaPetra <80599236+OlhaPetra@users.noreply.github.com>
2022-04-09 11:30:04 +0300

    Add token registration

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,7 +1,13 @@
 const CreateError = require("http-errors");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { v4: uuidv4 } = require("uuid");
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 const { User, schemas } = require("../../models/user");
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res, next) => {
   try {
@@ -17,15 +23,22 @@ const register = async (req, res, next) => {
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
 
+    const payload = {
+      id: uuidv4(),
+    };
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+
     await User.create({
       email,
       password: hashPassword,
       name,
+      token,
     });
 
     res.status(201).json({
       status: "success",
       code: 201,
+      token,
       user: {
         email,
         name,
