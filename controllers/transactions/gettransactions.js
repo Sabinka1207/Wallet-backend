573cb5ba3f18d0c15ca608ec8d3d99c327ebb3bf
tskalska <tatiana.skalska@gmail.com>
2022-04-13 01:10:15 +0200

    fix:sort_transactions

--- controllers/transactions/gettransactions.js
+++ controllers/transactions/gettransactions.js
@@ -3,13 +3,11 @@ const {Transaction} = require("../../models/transaction");
 const gettransactions = async (req, res, next) => {
     const {_id} = req.user;
     console.log()
-    const transactions = await Transaction.find({owner:_id}).populate("category","type nameDropdown nameStatistics").sort({ 'date' : -1 }).limit(6);
+    const transactions = await Transaction.find({owner:_id}).populate("category","type nameDropdown nameStatistics").sort({ 'createdAt' : -1 }).limit(6);
       res.json({
       status: 'success',
       code: 200,
-      data:  {
-        response: transactions
-    }
+      transactions
   })
 }
 
