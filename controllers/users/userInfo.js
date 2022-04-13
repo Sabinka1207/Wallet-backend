89e2509711adf8f2485defd0df24b2a62c8c4542
Olha Dolzhenko <olyasorokolat@gmail.com>
2022-04-13 15:37:14 -0400

    userInfo rolled back

--- controllers/users/userInfo.js
+++ controllers/users/userInfo.js
@@ -1,31 +1,21 @@
-//const CreateError = require("http-errors");
-
 const { Transaction } = require("../../models/transaction");
 
 const getUserInfo = async (req, res, next) => {
   try {
     const { _id, name, token, balance } = req.user;
     const query = { owner: _id };
-    let transactions;
-    /* let currentBalance = "0.0"; */
-    const recentTransactions = await Transaction.find(
+    const transactions = await Transaction.find(
       query,
       "-createdAt -updatedAt"
     )
       .sort({ date: -1 })
       .limit(6);
-    if (!recentTransactions) {
-      transactions = {};
-    } else {
-      transactions = recentTransactions;
-      /*  currentBalance = recentTransactions[0].currentBalance; */
-    }
+
     res.json({
       _id,
       name,
       token,
       balance,
-      /* currentBalance, */
       transactions,
     });
   } catch (error) {
